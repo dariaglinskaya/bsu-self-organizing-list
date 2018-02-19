297a348e35c5be9cea995900408268eae43d9279
dariaglinskaya <fpm.glinskaya@gmail.com>
2018-02-19 14:44:00 +0300

    implementing list

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -1,8 +1,10 @@
+'use strict'
 class Node {
     constructor(data) {
         this.data = data;
-        this.next = null;
         this.prev = null;
+        this.next = null;
+
     }
 }
 
@@ -11,42 +13,171 @@ class SelfOrganizedList {
         this.head = null;
         this.tail = null;
     }
-
-    insert(data) {
-
-    }
-
     size() {
+        if(this.head == null){
+            console.log("head is null length is 0");
+            return 0;
+        }
 
+        let count = 0;
+        let node = this.head;
+        while(node != null){
+            count ++;
+            node = node.next;
+        }
+        return count;
     }
-
+    insert(data) {
+        
+        var node = new Node(data);
+        if(!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+        return node;
+    }
     at(index) {
-
+        let currentNode = this.head;
+        let size = this.size();
+        let count = 0;
+        if (size === 0 || index < 1 || index > this.size()) {
+            return null;
+        }
+        while (count < index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+        return currentNode.data;
     }
-
     findNode(data) {
+        let currentNode = this.head;
 
+        while (currentNode) {
+            if (currentNode.data === data)
+                return currentNode;
+            currentNode = currentNode.next;
+        }
+        if (currentNode === null)
+            return null;
     }
-
     toArray() {
-
+        let result = [];
+        let current = this.head;
+        while(current){
+            result.push(current.data);
+            current = current.next;
+        }
+        return result;
     }
-
     removeAt(index) {
+        if (index > -1 && index < this.size()){
+            let current = this.head;
+            let i = 0;
 
+            //special case: removing first item
+            if (index === 0){
+                this.head = current.next;
+                if (!this.head){
+                    this.tail = null;
+                } else {
+                    this.head.prev = null;
+                }
+            } else if (index === this.size() -1){
+                current = this.tail;
+                this.tail = current.prev;
+                this.tail.next = null;
+            } else {
+
+                //find the right location
+                while(i++ < index){
+                    current = current.next;
+                }
+
+                //skip over the item to remove
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+
+            //return the value
+            return current;
+
+        } else {
+            return null;
+        }
     }
-
     moveToFront(node) {
-
+        if(this.head === node){
+            return;
+        }else if(this.tail === node){
+            node.prev.next = null;
+            this.tail = node.prev;
+        }else{
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        this.head.prev = node;
+        node.next = this.head;
+        node.prev = null;
+        this.head = node;
     }
-
     reorganize(data) {
+        let node = this.findNode(data);
+        if(!node){
+            return false;
+        }else{
+            this.moveToFront(node);
+            return true;
+        }
 
     }
-
 }
 
 module.exports = {
     SelfOrganizedList,
     Node
 };
+
+const sol = new SelfOrganizedList();
+
+sol.insert('Game of gnomes');
+sol.insert('Superficial');
+sol.insert('Breaking good');
+//console.log(sol);
+
+//++++++++++
+console.log('Size');
+console.log(sol.size()); // 3
+
+//++++++++++
+console.log('At');
+console.log(sol.at(1)); // Superficial
+
+
+//+++++++++++
+//++++++++++
+console.log('ToArray');
+console.log(sol.toArray()); // ['Game of gnomes', 'Superficial', 'Breaking good']
+
+
+
+//+++++++++++
+console.log('Remove');
+//sol.removeAt(1);
+//console.log(sol);
+
+//+++++++++++
+console.log('Find node');
+//console.log(sol.findNode('Breaking good'));
+
+
+//
+//console.log(sol.moveToFront(sol.tail));
+//console.log(sol.toArray());
+//console.log(sol);
+//console.log(sol.reorganize('Breaking good')); // true, and reorganize list to 'Breaking good' -> 'Game of gnomes' -> 'Superficial'
+console.log(sol.reorganize('Breaking bad')); // false, list the same
+console.log(sol.toArray());
